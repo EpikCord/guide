0b603556e75c6f8f298a21e2d0b7299a0ed0677b
TheUntraceable <ridhwanplayzzz@gmail.com>
2022-02-23 18:30:44 +0000

    Remove Geeks4Geeks thingy. Yes I did source from there.

--- components/codeblock.js
+++ components/codeblock.js
@@ -4,7 +4,6 @@ import { CopyBlock, railscast } from "react-code-blocks";
 export default function CodeBlock(code) {
   return (
     <div>
-      <h3>GeeksforGeeks Code</h3>
       <CopyBlock
       text= {code}
       language='python'
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import CodeBlock from "../components/codeblock"
 import styles from "../styles/Home.module.css"
 
+const code = "print("
+
 export default function Home() {
     return (
         <div>
@@ -13,6 +15,8 @@ export default function Home() {
             <div className = {styles.text}>
                 <p>This guide will help you get started using EpikCord.py as there is no documentation right now.</p>
                 <p>If you have any questions, feel free to contact us on Discord <a target = "blank" href="https://discord.gg/4R473R73kQ">here</a>!</p>
+
+                <p>TL;DR, the most basic bot uses the following code: {CodeBlock(code)}</p>
             </div>
  
         </div>
