1f3b6d97d4ac8c9705e81e83ccf0b30163a4c1f6
TheUntraceable <ridhwanplayzzz@gmail.com>
2022-02-24 19:13:05 +0000

    Fix issue?

--- pages/getting-started.js
+++ pages/getting-started.js
@@ -3,7 +3,7 @@ import Head from "../components/head";
 export default function GettingStarted() {
     return (
         <html>
-            <Head pageTitle = "EpikCord.py Guide - Getting Started" pageDescription = "Want to get started? Read this!" />
+            {Head("EpikCord.py Guide - Getting Started", "Want to get started? Read this!")}
         </html>
     )
 }
