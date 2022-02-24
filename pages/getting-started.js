e31844be38b45c1e16cfea99614c27fbb5457584
TheUntraceable <ridhwanplayzzz@gmail.com>
2022-02-24 19:23:01 +0000

    Current State

--- pages/getting-started.js
+++ pages/getting-started.js
@@ -4,6 +4,13 @@ export default function GettingStarted() {
     return (
         <html>
             {Head("EpikCord.py Guide - Getting Started", "Want to get started? Read this!")}
+            <div>
+                <h1>EpikCord.py Guide - Getting Started</h1>
+                <p>
+                    So on this page, I will try my hardest to explain the basics of EpikCord.py.
+                    Not everything is perfect so feel free to ask me questions in the <a target = "blank" href="https://discord.gg/4R473R73kQ">Discord</a>.
+                </p>
+            </div>
         </html>
     )
 }
